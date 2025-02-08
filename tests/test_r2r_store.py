2e63d4c8953158f0f21ec5d21ef3dd197bceed81
Gleb Frank <gleb@glebfrank.com>
2025-02-08 11:53:40 -0800

    Added Northwind data for tests.

--- tests/test_r2r_store.py
+++ tests/test_r2r_store.py
@@ -1,10 +1,10 @@
 import logging
 import re
-from typing import Mapping
+from typing import Mapping, Type
 import unittest
 from sqlalchemy import create_engine, Engine, Connection, text
 
-from rdflib import Graph, URIRef
+from rdflib import Graph, Namespace, URIRef
 from rdflib_r2r.new_r2r_store import NewR2rStore
 from rdflib_r2r.r2r_mapping import R2RMapping
 
@@ -13,6 +13,9 @@ def norm_ws(s:str|None) -> str|None:
         return None
     return re.sub(r'\s+', ' ', s).strip()
 
+DEMO_NS = Namespace("http://localhost:8890/Demo/")
+OD_NS = Namespace(DEMO_NS["orders/"])
+
 class TestR2RStore(unittest.TestCase):
 
     db: Engine
@@ -20,37 +23,39 @@ class TestR2RStore(unittest.TestCase):
     mapping: R2RMapping
     ns_map: Mapping[str, URIRef]
 
-    @classmethod
-    def setUpClass(cls):
-        cls.db = create_engine("sqlite+pysqlite:///:memory:")
-        cls.conn = cls.db.connect()
-        with open('tests/data/CreateTables.sql') as f:
+    @staticmethod
+    def setup_db(target:"TestR2RStore|Type[TestR2RStore]"):
+        target.db = create_engine("sqlite+pysqlite:///:memory:")
+        target.conn = target.db.connect()
+        with open('tests/data/Northwind.sql') as f:
             for stmt in f.read().split(';'):
-                cls.conn.execute(text(stmt))
+                target.conn.execute(text(stmt))
         r2rg = Graph()
-        r2rg.parse('tests/data/GoInspireR2RML.ttl')
-        cls.mapping = R2RMapping(r2rg)
-        cls.ns_map = {}
+        r2rg.parse('tests/data/NorthwindR2RML.ttl')
+        target.mapping = R2RMapping(r2rg)
+        target.ns_map = {}
         for prefix, ns in r2rg.namespaces():
-            cls.ns_map[prefix] = URIRef(ns)
+            target.ns_map[prefix] = URIRef(ns)
+        
 
     def setUp(self):
+        TestR2RStore.setup_db(self)
         self.store = NewR2rStore(self.db, self.mapping)
 
     def check(self, sparql:str, expected_sql:str|None):
-        print("SPARQL:", sparql)
-        print("Expected SQL:", expected_sql)
+        # print("SPARQL:", sparql)
+        # print("Expected SQL:", expected_sql)
         actual_sql = self.store.getSQL(sparql, initNs=self.ns_map)
-        print("Actual SQL:", actual_sql)
+        # print("Actual SQL:", actual_sql)
         self.assertEqual(norm_ws(actual_sql), norm_ws(expected_sql))
 
     def test_concrete_order_value(self):
-        self.check('select ?v { <http://daax.ai/GoInspire/sample/order_header/Order_ID/1#this> gi:orderValue ?v}',
-                   'SELECT t0."Order_Value" AS v\nFROM order_header AS t0\nWHERE t0."Order_ID" = 1')
+        self.check(f'select ?v {{ <{OD_NS}1> Demo:freight ?v}}',
+                   'SELECT t0."Freight" AS v FROM "Orders" AS t0\nWHERE t0."OrderID" = 1')
 
     def test_concrete_order_concrete_value(self):
-        self.check('select (1 as ?k) { <http://daax.ai/GoInspire/sample/order_header/Order_ID/1#this> gi:orderValue 3.50}',
-                   'SELECT 1 AS k\nFROM order_header AS t0\nWHERE t0."Order_ID" = 1 AND t0."Order_Value" = 3.50')
+        self.check(f'select (1 as ?k) {{ <{OD_NS}1> Demo:freight 3.50}}',
+                   'SELECT 1 AS k\nFROM "Orders" AS t0\nWHERE t0."OrderID" = 1 AND t0."Freight" = 3.50')
 
 
 if __name__ == '__main__':
